ba40cb6d18f366531c9a649cce13dab70db6e9e6
sauravkumarburnwal36 <sauravkumarburnwal36@gmail.com>
2025-01-27 19:09:11 +0530

    Added Default Spring Caching

---
+++ src/main/java/com/example/caching/CachingDemo/services/EmployeeService.java
@@ -0,0 +1,12 @@
+package com.example.caching.CachingDemo.services;
+
+
+import com.example.caching.CachingDemo.dto.EmployeeDto;
+
+public interface EmployeeService {
+
+    EmployeeDto getEmployeeById(Long id);
+    EmployeeDto createNewEmployee(EmployeeDto employeeDto);
+    EmployeeDto updateEmployee(Long id, EmployeeDto employeeDto);
+    void deleteEmployee(Long id);
+}
